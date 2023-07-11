9cf38c35cea3a1f5d54f9fac6d0f847d15e249df
achakpoisioma <achakpoisioma06@gmail.com>
2023-07-11 14:54:28 -0700

    2-str_concat.c git commit -m 2-str_concat.c
    betty 2-str_concat.c
    
    clear
    
    git commit -m 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * str_concat - get ends of input and add together for size
  * @s1: input one to concat
@@ -16,12 +15,14 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-		i = ci = 0;
+
+	i = ci = 0;
 	while (s1[i] != '\0')
 		i++;
 	while (s2[ci] != '\0')
 		ci++;
 	conct = malloc(sizeof(char) * (i + ci + 1));
+
 	if (conct == NULL)
 		return (NULL);
 	i = ci = 0;
@@ -30,6 +31,7 @@ char *str_concat(char *s1, char *s2)
 		conct[i] = s1[i];
 		i++;
 	}
+
 	while (s2[ci] != '\0')
 	{
 		conct[i] = s2[ci];
@@ -38,3 +40,4 @@ char *str_concat(char *s1, char *s2)
 	conct[i] = '\0';
 	return (conct);
 }
+
