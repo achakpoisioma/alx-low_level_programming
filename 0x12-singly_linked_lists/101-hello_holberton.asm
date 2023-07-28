a8e8f1d8b9559a3ae2b0fee69e3ac6ebb67c9af7
achakpoisioma <achakpoisioma06@gmail.com>
2023-07-27 18:34:07 -0700

    task 6

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+; 101-hello_holberton.asm - Print a string using printf
+; by Achakpo Isioma
+; Assemble: nasm -f elf64 101-hello_holberton.asm
+; Link: gcc -no-pie -std=gnu89 101-hello_holberton.o -o hello
+
+	SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+	SECTION .text
+	extern printf
+	global main
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
