841ce2a0a640413f84a15b625d7d6bb53de8bc33
achakpoisioma <achakpoisioma06@gmail.com>
2023-07-27 18:45:52 -0700

    task 6 fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-#include "lists.h"
 
 /**
- * before_main - funcion that prints a specific string before the main
+ * first - prints a sentence before the main
  * by Achakpo Isioma
  *
  * function is executed
  *
- * return: no return
+ * rutun: no return
  */
-void before_main(void)
+void __attribute__ ((constructor)) first()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
-
